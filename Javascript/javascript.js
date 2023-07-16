ad7ad90f730f5dee56321616dcaf60f217cf5478
Anil chauhan <132829274+Anilchauhan24@users.noreply.github.com>
2023-07-16 15:24:57 +0530

    Add files via upload

---
+++ Javascript/javascript.js
@@ -0,0 +1,40 @@
+//var a = "Anil";
+//assignment value is 10 and declaration is a;//
+var b= 40;
+var c= 50;
+d= b+c;//
+console.log(c);
+console.log(typeof d);
+var e= 50.60;
+console.log(typeof e);
+
+var a="chuhan";
+console.log(typeof a);
+
+var f= true;
+console.log(typeof f);
+
+var a=50;
+var c= 60;
+ d= a*c;
+ console.log(d);
+
+ var a= 5;
+ var b= 6;
+  d=a%b;
+  console.log(d);
+
+  var a ="Anil";
+  var z="Chauhan";
+   c= a+z;
+   console.log(c);
+
+   var d="aNIL";
+   var x= "chauhan";
+    y= d*x;
+    console.log(y);
+
+    var f= "Rohin";
+     var g= "Samba";
+      h= f-g;
+      console.log(h);
